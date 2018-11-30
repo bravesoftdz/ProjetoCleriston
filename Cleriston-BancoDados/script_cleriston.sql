7ebbcd68c69b89c103cf349c090f1b1c47484c15
DESKTOP-IR1GLG9\vitor <vitor.carareto@gmail.com>
2018-11-30 20:57:14 -0200

    Alterado sintaxe de MySQL para PostgreSQL

--- Cleriston-BancoDados/script_cleriston.sql
+++ Cleriston-BancoDados/script_cleriston.sql
@@ -1,41 +1,40 @@
-create database trabalhos;
 
-use trabalhos;
-
-create table Pessoas(
-id  int     not null    primary key     auto_increment,
-nome    varchar(50) not null
+create table if not exists Pessoas(
+  id serial primary key,
+  nome varchar(50) not null
 );
 
-create table Alunos(
-id_pessoa int primary key not null references Pessoas(id),
-ra  int not null);
-
-create table Professores(
-id_pessoa int primary key not null references Pessoas(id),
-salario decimal(10,2) not null
+create table if not exists Alunos(
+  id_pessoa int primary key not null references Pessoas(id),
+  ra int not null
 );
 
-create table Disciplinas(
-    id  int not null    primary key auto_increment,
-    id_professor int not null references Professores(id),
-    nome    varchar(50) not null
+create table if not exists Professores(
+  id_pessoa int primary key not null references Pessoas(id),
+  salario decimal(10,2) not null
 );
 
-create table Atribuicoes(
-    id_aluno int not null references Alunos(id),
-    id_disciplina int not null references Disciplinas(id),
-    primary key (id_aluno, id_disciplina) 
+create table if not exists Disciplinas(
+  id serial primary key,
+  id_professor int not null references Professores(id_pessoa),
+  nome varchar(50) not null
 );
 
-create table Trabalhos(
-id  int not null primary key auto_increment,
-arquivo blob,
-nome varchar(50) not null,
-descricao varchar(200),
-nota_trabalho decimal(4,2),
-obs_trabalho varchar(200),
-id_disciplina int not null    references Disciplinas(id)
+create table if not exists Atribuicoes(
+  id_aluno int not null references Alunos(id_pessoa),
+  id_disciplina int not null references Disciplinas(id),
+  primary key (id_aluno, id_disciplina) 
 );
+
+create table if not exists Trabalhos(
+  id serial primary key,
+  arquivo bytea,
+  nome varchar(50) not null,
+  descricao varchar(200),
+  nota_trabalho decimal(4,2),
+  obs_trabalho varchar(200),
+  id_disciplina int not null references Disciplinas(id)
+);
+
 
 
