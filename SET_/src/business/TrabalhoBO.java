db1181d346b12c4bd6074c2f562491d168f3bf88
joaolucasfachini <jl_fachini@hotmail.com>
2018-11-30 21:13:08 -0200

    alteraçao nas classes BO

--- SET_/src/business/TrabalhoBO.java
+++ SET_/src/business/TrabalhoBO.java
@@ -1,27 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package business;
 
-import java.util.ArrayList;
-import java.util.List;
 import entities.Trabalho;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Cleriston
+ */
 public class TrabalhoBO {
-	
-	private static List<Trabalho> trabalhoList= new ArrayList<Trabalho>();
     
-    public static void inserirTrabalho(Trabalho novoTrabalho){
-        getTrabalhoList().add(novoTrabalho);
+    private List<Trabalho> trabalhoList= new ArrayList<Trabalho>();
+
+    
+    private Connection connection = null;
+
+    public Connection getConnection() {
+    	 String url = "jdbc:postgresql://trabalhoscleriston.cm71a7r8urep.sa-east-1.rds.amazonaws.com:5432/trabalhos";
+         String username = "root";
+         String password = "trabalho1234";
+         try {
+             Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new IllegalStateException("Não é possível achar o driver!", e);
+        }
+        
+        try {
+            connection = DriverManager.getConnection(url, username, password);
+        } 
+         catch (SQLException e) {
+            throw new IllegalStateException("Não foi possível conectar ao banco de dados.", e);
+        }
+        return connection;
+    }
+
+    public void setConnection(Connection connection) {
+        this.connection = connection;
     }
     
-    public List<Trabalho> recuperarTodostrabalhos(){
+    public void inserirAluno(Trabalho novoTrabalho){
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement("insert into set.trabalhos (nome) VALUES (?);");
+            stmt.setString(1, novoTrabalho.getNome());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    public List recuperarTodosTrabalhos(){
+    	trabalhoList.clear();
+        try {
+            Statement stm = getConnection().createStatement();
+            ResultSet rs = stm.executeQuery("SELECT * FROM  set.trabalhos");
+            while (rs.next()) {
+            	Trabalho trabalho = new Trabalho();
+
+            	trabalho.setId(rs.getLong(1));
+            	trabalho.setNome(rs.getString(2));
+            	trabalho.setConteudo(rs.getString(3));
+            	trabalho.setNota(rs.getFloat(4));
+            	trabalho.setIdDisciplina(idDisciplina);
+            	
+            	trabalhoList.add(trabalho);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return getTrabalhoList();
     }
+    
+   public Trabalho buscarTrabalhoById(String id){
+	   trabalhoList.clear();
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement("SELECT * FROM  set.trabalhos  where id = ?");
+            stmt.setString(1, id);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+            	Trabalho trabalho = new Trabalho();
 
-	public static List<Trabalho> getTrabalhoList() {
-		return trabalhoList;
-	}
+            	trabalho.setId(rs.getLong(1));
+            	trabalho.setNome(rs.getString(2));
+            	trabalho.setRa(rs.getString(3));
+                return trabalho;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+    
+    public List pesquisaPorNome(String paramPesquisa){
+    	trabalhoList.clear();
+        try {
+            PreparedStatement stmt = getConnection().prepareStatement("SELECT * FROM  set.trabalhos  where nome like ?");
+            stmt.setString(1, "%"+paramPesquisa+"%");
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+            	Trabalho trabalho = new Trabalho();
 
-	public void setTrabalhoList(List<Trabalho> trabalhoList) {
-		TrabalhoBO.trabalhoList = trabalhoList;
-	}
+            	Trabalho.setId(rs.getLong(1));
+            	Trabalho.setNome(rs.getString(2));
+            	trabalhoList.add(trabalho);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return getTrabalhoList();
+    }
+    
+    public void removerAluno(Trabalho trabalho){
+         try {
+            PreparedStatement stmt = getConnection().prepareStatement("delete from set.trabalhos where id=?;");
+            stmt.setLong(1, trabalho.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    public void atualizarAluno(Trabalho trabalho){
+         try {
+            PreparedStatement stmt = getConnection().prepareStatement("UPDATE set.trabalhos SET nome=? where id=?;");
+            stmt.setString(1, trabalho.getNome());
+            stmt.setLong(2, trabalho.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    
+    //Getters and setters
+    public List<Trabalho> getTrabalhoList() {
+        return trabalhoList;
+    }
 
+    public void setAlunoList(List<Trabalho> alunoList) {
+        this.trabalhoList = alunoList;
+    }
+    
+
+    
 }
--- SET_/src/entities/Trabalho.java
+++ SET_/src/entities/Trabalho.java
@@ -1,16 +1,43 @@
 package entities;
 
 public class Trabalho {
-
+	private long id;
 	private String nome;
-
+	private String conteudo; 
+	private float nota;
+	private long idDisciplina;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public String getConteudo() {
+		return conteudo;
+	}
+	public void setConteudo(String conteudo) {
+		this.conteudo = conteudo;
+	}
+	public float getNota() {
+		return nota;
+	}
+	public void setNota(float nota) {
+		this.nota = nota;
+	}
+	public long getIdDisciplina() {
+		return idDisciplina;
+	}
+	public void setIdDisciplina(long idDisciplina) {
+		this.idDisciplina = idDisciplina;
+	}
+	
+
 	
 	
 }
